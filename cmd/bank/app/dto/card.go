d114568cbad0ad2d1c4ad6516259629fa2e5826c
Andrey Lozovoy <lozovoya@gmail.com>
2020-09-13 17:43:49 +0300

    dto modified

--- cmd/bank/app/dto/card.go
+++ cmd/bank/app/dto/card.go
@@ -1,12 +1,12 @@
 package dto
 
 type CardDTO struct {
-	Id       int    `json:"id"`
-	Number   string `json:"number"`
-	Issuer   string `json:"issuer"`
-	HolderId int    `json:"holder_id"`
-	Type     string `json:"type"`
-	Message  string `json:"message"`
+	Id       int    `json:"id,omitempty"`
+	Number   string `json:"number,omitempty"`
+	Issuer   string `json:"issuer,omitempty"`
+	HolderId int    `json:"holder_id,omitempty"`
+	Type     string `json:"type,omitempty"`
+	Message  string `json:"message,omitempty"`
 }
 
 type MessageDTO struct {
